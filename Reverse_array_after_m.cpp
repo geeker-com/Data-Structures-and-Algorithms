d6efbf893db00b6226df6768661d59ded5711721
Harshit Pokhriyal <harshitpokhriyal1729@gmail.com>
2022-12-22 23:24:12 +0530

    Reverse array after m index

---
+++ Reverse_array_after_m.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverseArray(vector<int> &arr, int m)
+{
+
+    int sz = arr.size();
+    int j = 1;
+
+    for (int i = m + 1; i < sz; i++)
+
+    {
+        int t = arr[i];
+        arr[i] = arr[sz - j];
+        arr[sz - j] = t;
+
+        if (j == (sz - m - 1) / 2)
+        {
+
+            break;
+        }
+        j++;
+    }
+}
+
+int main()
+{
+    vector<int> arr;
+    arr = {1, 2, 3, 4, 5, 6};
+    reverseArray(arr, 3);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
