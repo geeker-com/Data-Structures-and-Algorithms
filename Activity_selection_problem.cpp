8b750a197154cd357d10c3cc1b727220d40882e1
Harshit Pokhriyal <harshitpokhriyal1729@gmail.com>
2022-12-31 18:15:10 +0530

    commit

--- Activity_selection_problem.cpp
+++ Activity_selection_problem.cpp
@@ -1,20 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+// int maxMeetings(int start[], int end[], int n)
+// {
+//     //writtten myself in this we didn't needed sorting by making custom
+//     //comparator
+//     vector<pair<int, int>> v;
+//     for (int i = 0; i < n; i++)
+//     {
+//         v.push_back(make_pair(end[i], start[i]));
+//     }
+//     sort(v.begin(), v.end());
+//     int c = 1;
+//     int ans = v[0].first;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         if (ans < v[i + 1].second)
+//         {
+//             ans = v[i + 1].first;
+//             c++;
+//         }
+//     }
+//     return c;
+// }
+static bool cmp(pair<int, int> a, pair<int, int> b)
+{
+    //always make it static while writing
+    return a.second < b.second;
+}
 int maxMeetings(int start[], int end[], int n)
 {
+    // writtten myself in this we didn't needed sorting by making custom
+    // comparator
     vector<pair<int, int>> v;
     for (int i = 0; i < n; i++)
     {
-        v.push_back(make_pair(end[i], start[i]));
+        v.push_back({start[i], end[i]});
     }
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), cmp);
     int c = 1;
-    int ans = v[0].first;
+    int ans = v[0].second;
     for (int i = 0; i < n - 1; i++)
     {
-        if (ans < v[i + 1].second)
+        if (ans < v[i + 1].first)
         {
-            ans = v[i + 1].first;
+            ans = v[i + 1].second;
             c++;
         }
     }
